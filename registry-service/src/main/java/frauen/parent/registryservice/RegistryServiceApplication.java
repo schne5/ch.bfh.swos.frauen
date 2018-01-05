7325f81bd5d1addf9c4f21b09bda76f128c4b710
schne5 <elisa@ElisasPc.bfh.ch>
2018-01-05 10:54:34 +0100

    changed dependency

--- registry-service/src/main/java/frauen/parent/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/frauen/parent/registryservice/RegistryServiceApplication.java
@@ -2,9 +2,10 @@ package frauen.parent.registryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryServer
+@EnableEurekaServer
 public class RegistryServiceApplication {
 
 	public static void main(String[] args) {
